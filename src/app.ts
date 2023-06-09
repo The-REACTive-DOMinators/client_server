b20424d7b3e83bc101ba961ffdec6a588793284e
Marko Boiko <106397016+VenomMB@users.noreply.github.com>
2023-06-09 12:21:25 +0300

    create remote database (#2)
    Co-authored-by: Mark Boiko <boikomark555@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import { dbInit } from './utils/dbInit';
 
 const app: Express = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
+dbInit();
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello World!' });
---
+++ src/utils/dbInit.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from 'sequelize';
+
+const URI =
+  'postgres://teamonereact:f1OFwSK0zGbW@ep-odd-leaf-749238.eu-central-1.aws.neon.tech/neondb';
+
+export const dbInit = () => {
+  try {
+    return new Sequelize(URI, {
+      dialectOptions: {
+        ssl: true
+      }
+    });
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
