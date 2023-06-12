9be7b3daec837133ec6cfebdf140e358bb697497
Denys Krysanov <denis.krisanov321@gmail.com>
2023-06-12 14:09:10 +0300

    C:/Program Files/Git/get methods added

--- dist/src/app.js
+++ dist/src/app.js
@@ -3,15 +3,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.startApp = void 0;
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const dbInit_1 = require("./utils/dbInit");
-const app = (0, express_1.default)();
-app.use(body_parser_1.default.urlencoded({ extended: false }));
-app.use(body_parser_1.default.json());
-app.use((0, cors_1.default)());
-(0, dbInit_1.dbInit)();
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' });
-});
+const products_1 = require("./routes/products");
+const startApp = () => {
+    const app = (0, express_1.default)();
+    app.use(body_parser_1.default.json());
+    app.use((0, cors_1.default)());
+    app.use('/phones', products_1.productsRoutes);
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+    (0, dbInit_1.dbInit)();
+};
+exports.startApp = startApp;
--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,16 @@ import cors from 'cors';
 import { dbInit } from './utils/dbInit';
 import { productsRoutes } from './routes/products';
 
-export const startApp = async () => {
+export const startApp = () => {
   const app: Express = express();
 
   app.use(bodyParser.json());
   app.use(cors());
-
   app.use('/phones', productsRoutes);
+
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
 
   dbInit();
 };
