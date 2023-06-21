3e2ec37c1c7807196da9352033d32c80b6dbb813
Marko Boiko <106397016+VenomMB@users.noreply.github.com>
2023-06-21 11:54:53 +0300

    Add new photos (#25)
    * add new photo
    
    * new photos
    
    ---------
    
    Co-authored-by: Mark Boiko <boikomark555@gmail.com>

--- build/src/routes/hotPrices.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hotPricesRoutes = void 0;
-const express_1 = require("express");
-const hotPrices_1 = require("../controllers/hotPrices");
-exports.hotPricesRoutes = (0, express_1.Router)();
-exports.hotPricesRoutes.get('/', hotPrices_1.hotPricesController.getHotPrices);
--- build/src/routes/products.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.productsRoutes = void 0;
-const express_1 = require("express");
-const products_1 = require("../controllers/products");
-exports.productsRoutes = (0, express_1.Router)();
-exports.productsRoutes.get('/phones', products_1.productsController.getAllProducts);
-exports.productsRoutes.get('/:id', products_1.productsController.getProductById);
