37f027e0f269a43eea4fc2d8018abb5d49eaabe3
Mark Boiko <boikomark555@gmail.com>
2023-06-21 11:47:06 +0300

    new photos

--- build/src/routes/newest.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.newestRoutes = void 0;
-const express_1 = require("express");
-const newest_1 = require("../controllers/newest");
-exports.newestRoutes = (0, express_1.Router)();
-exports.newestRoutes.get('/', newest_1.newestController.getNewest);
--- build/src/routes/phones.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.phonesRoutes = void 0;
-const express_1 = require("express");
-const phones_1 = require("../controllers/phones");
-exports.phonesRoutes = (0, express_1.Router)();
-exports.phonesRoutes.get('/count', phones_1.phonesController.getCountOfPhone);
-exports.phonesRoutes.get('/:phoneId', phones_1.phonesController.getPhoneById);
-exports.phonesRoutes.get('/:phoneId/recommended', phones_1.phonesController.getRecommended);
